d1fb77088341fa6e50e4c6617b9a5f22d58c0114
tzz <1789803837@qq.com>
2018-05-30 22:14:55 +0800

    2

--- src/FR/FaceRecogintion.java
+++ src/FR/FaceRecogintion.java
@@ -5,41 +5,90 @@ import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+import java.awt.*;
+
 public class FaceRecogintion {
 
-    public Mat getGrayImgFromMat(Image image) {
+    FaceRecogintion() {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public Mat getGrayMatFromImg(Image image) {
         if (image != null) {
             PixelReader pixelReader = image.getPixelReader();
-            Mat imageMat = new Mat((int) image.getHeight(), (int) image.getWidth(), CvType.CV_32F);
-            for (int i = 0; i < image.getHeight(); i++) {
-                for (int j = 0; j < image.getWidth(); j++) {
-                    Color color = pixelReader.getColor(i, j);
-                    float gray = (float) ((color.getBlue() + color.getGreen() + color.getRed()) / 3);
-                    imageMat.put(j, i, gray);
+            Mat matImage = new Mat((int) (image.getHeight()), (int) (image.getWidth()), CvType.CV_32F);
+            //遍历源图像每个像素，将其写入到目标图像
+
+            for (int y = 0; y < image.getHeight(); y++) {
+                for (int x = 0; x < image.getWidth(); x++) {
+                    Color color = pixelReader.getColor(x, y);
+                    float gray = (float) ((color.getBlue() + color.getGreen() + color.getRed()) / 3.0);
+                    matImage.put(y, x, gray);
                 }
             }
-            return imageMat;
+            return matImage;
         }
         return null;
     }
 
-    public Image getImgFromMat(Mat imageMat) {
-        if (imageMat != null) {
-            WritableImage writableImage = new WritableImage(imageMat.height(), imageMat.width());
+    public Image getImgFromMat(Mat matImg) {
+        if (matImg != null) {
+            WritableImage wImage = new WritableImage(matImg.width(), matImg.height());
+            //得到像素写入器
+            PixelWriter pixelWriter = wImage.getPixelWriter();
+            if (matImg.channels() == 1) {  //单通道图像
+                float[] gray = new float[matImg.height() * matImg.width()];
+                matImg.get(0, 0, gray);
+                //遍历源图像每个像素，将其写入到目标图像
+                for (int y = 0; y < matImg.height(); y++) {
+                    for (int x = 0; x < matImg.width(); x++) {
+                        int pixelIndex = y * matImg.width() + x;
+                        Color color = Color.gray(gray[pixelIndex]);
+                        pixelWriter.setColor(x, y, color);
+                    }
+                }
+            } else if (matImg.channels() == 3) {    //3通道图像
+                //遍历源图像每个像素，将其写入到目标图像
+                for (int y = 0; y < matImg.height(); y++) {
+                    for (int x = 0; x < matImg.width(); x++) {
+                        int[] gray = new int[3];
+                        matImg.get(y, x, gray);
+                        Color color = Color.rgb(gray[2], gray[1], gray[0]);
+                        pixelWriter.setColor(x, y, color);
+                    }
+                }
+            }
+            return wImage;
+        }
+        return null;
+    }
+
+
+    public Image getResizeImg(Image image, Point begin, Point end, double zoom_multiples) {
+        if (image != null) {
+            PixelReader pixelReader = image.getPixelReader();
+//            Mat matImage = new Mat((int) (image.getHeight()), (int) (image.getWidth()), CvType.CV_32F);
+
+            WritableImage writableImage = new WritableImage((int) (image.getHeight()), (int) (image.getWidth()));
             PixelWriter pixelWriter = writableImage.getPixelWriter();
-            float[] gray = new float[imageMat.height() * imageMat.width()];
-            imageMat.get(0, 0, gray);
-            for (int y = 0; y < imageMat.height(); y++) {
-                for (int x = 0; x < imageMat.width(); x++) {
-                    Color color = Color.gray(gray[y * imageMat.width() + x]);
-                    pixelWriter.setColor(x, y, color);
+
+            for (int y = begin.y; y < end.y; y++) {
+                for (int x = begin.x; x < end.x; x++) {
+                    Color color = pixelReader.getColor(x, y);
+                    double x1 = color.getBlue();
+                    double x2 = color.getRed();
+                    double x3 = color.getGreen();
+                    Color color2 = new Color(x2, x3, x1, 1);
+                    pixelWriter.setColor(x, y, color2);
                 }
             }
             return writableImage;
         }
         return null;
     }
+
 }
--- src/FR/UIController.java
+++ src/FR/UIController.java
@@ -5,11 +5,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.opencv.core.Mat;
 
+import java.awt.*;
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 public class UIController {
     @FXML
@@ -18,8 +25,23 @@ public class UIController {
     private Button bt2;
     @FXML
     private ImageView img1;
+    @FXML
+    private ImageView img2;
+    @FXML
+    private Pane pane1;
+    @FXML
+    private Pane pane_img1;
 
-    File file = null;
+    private File file = null;
+    private Image image = null;
+    private boolean click = false;
+    private double p1_x;
+    private double p1_y;
+    private double p2_x;
+    private double p2_y;
+    private int clickCount = 0;
+    private ArrayList<Circle> circles = new ArrayList<>();
+
 
     /***
      * 选择图片
@@ -36,7 +58,7 @@ public class UIController {
             try {
                 String path = file.toURI().toURL().toString();
                 System.out.println(path);
-                Image image = new Image(path);
+                image = new Image(path);
                 img1.setImage(image);
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -44,7 +66,59 @@ public class UIController {
         }
     }
 
-    public void normalize() {
+    public void displaynormalize(Point begin, Point end, double zoom_multiples) {
+        if (image != null) {
+            FaceRecogintion fr = new FaceRecogintion();
+            Image resizeImg = fr.getResizeImg(image, begin, end, zoom_multiples);
+            Mat mat = fr.getGrayMatFromImg(resizeImg);
+            Image imgFromMat = fr.getImgFromMat(mat);
 
+            img2.setImage(imgFromMat);
+        }
+    }
+
+    public void onMouseClick(MouseEvent event) {
+        if (img1 != null && click) {
+            clickCount++;
+            Circle circle = new Circle(event.getX(), event.getY(), 3);
+            circles.add(circle);
+            circle.setFill(Color.RED);
+            pane_img1.getChildren().add(circle);
+            if (clickCount == 2) {
+                p2_x = event.getX();
+                p2_y = event.getY();
+
+                double distance = Math.sqrt(Math.pow((p1_x - p2_x), 2) + Math.pow((p1_y - p2_y), 2));
+                double zoom_multiples = distance * 2 / 128;
+                double x, y;
+                Point begin = new Point();
+                Point end = new Point();
+                x = (p1_x + p2_x) / 2;
+                y = (p1_y + p2_y) / 2;
+
+
+                begin.x = (int) (x - distance);
+                begin.y = (int) (y - distance * 0.5);
+                end.x = (int) (x + distance);
+                end.y = (int) (y + distance * 1.5);
+
+
+
+
+                displaynormalize(begin, end, zoom_multiples);
+
+                for (Circle circle1 : circles) {
+                    pane_img1.getChildren().remove(circle1);
+                }
+            } else {
+                p1_x = event.getX();
+                p1_y = event.getY();
+            }
+        }
+
+    }
+
+    public void normalize(ActionEvent event) {
+        click = true;
     }
 }
